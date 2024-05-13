4735062912c29cc4252d2584a7e0ccb1670dc0b9
Bemin <bemin_dawoud@yahoo.com>
2024-05-13 05:18:42 -0700

    create 1-pack_web_static.py

---
+++ 1-pack_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+generates a .tgz archive from the contents of the web_static folder
+"""
+from fabric.api import *
+from datetime import datetime
+
+def do_pack():
+    """
+    function to create the archive
+    """
+    try:
+        time = datetime.now().strftime("%Y%m%d%H%M%S")
+        local("mkdir -p versions")
+        archive_path = f"versions/web_static{time}.tgz"
+        local(f"tar -czvf {archive_path} web_static")
+        return archive_path
+    except:
+        return None
