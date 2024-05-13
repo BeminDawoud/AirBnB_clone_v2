19605c1c562a5f86ce4fa70713b97e0f89d5f08c
Bemin <bemin_dawoud@yahoo.com>
2024-05-13 04:21:54 -0700

    update 0-setup_web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,13 +2,13 @@
 # sets up web servers for the deployment of web_static
 sudo apt-get update 
 sudo apt-get -y install nginx
-mkdir -p /data/web_static/releases/test /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared/
 echo "Holberton School" >> /data/web_static/releases/test/index.html
 if [ -e "/data/web_static/current" ]; then
-	rm /data/web_static/current
+	sudo rm /data/web_static/current
 else
-	ln -s /data/web_static/releases/test/ /data/web_static/current
+	sudo ln -s -n /data/web_static/releases/test/ /data/web_static/current
 fi
-chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '/listen 80 default_server/a location \/hbnb_static{\n\talias \/data\/web_static\/current\/;\n}' /etc/nginx/sites-available/default
 service nginx restart
