a50f1a9caa782d5840c9cc45d95332d3d2f53e1a
Bemin <bemin_dawoud@yahoo.com>
2024-05-16 11:54:12 -0700

    update

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -3,22 +3,22 @@
 creates and distributes an archive to web servers
 """
 
-from fabric.api import *
+from fabric.api import env, local, put, run
 from datetime import datetime
-from os.path import exists
-env.hosts = ['18.204.3.147', '3.84.158.72']
+from os.path import exists, isdir
+
+env.hosts = ["18.204.3.147", "3.84.158.72"]
 
 
 def do_pack():
-    """
-    function to create the archive
-    """
+    """function to create the archive"""
     try:
-        time = datetime.now().strftime("%Y%m%d%H%M%S")
-        local("mkdir -p versions")
-        archive_path = f"versions/web_static_{time}.tgz"
-        local(f"tar -czvf {archive_path} web_static")
-        return archive_path
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -czvf {} web_static".format(file_name))
+        return file_name
     except:
         return None
 
@@ -28,27 +28,25 @@ def do_deploy(archive_path):
     if exists(archive_path) is False:
         return False
     try:
-        put(f"{archive_path}", "/tmp/")
-        archive_name = archive_path.split("/")[-1]
-        folder_name = archive_name.split(".")[0]
-        remote_path = f"/data/web_static/releases/{folder_name}"
-        run(f"sudo mkdir -p {remote_path}")
-        run(f"sudo tar -xzf /tmp/{archive_name} -C {remote_path}")
-        run(f"sudo mv {remote_path}/web_static/* {remote_path}")
-        run(f"sudo rm -rf {remote_path}/web_static")
-        run(f"sudo rm /tmp/{archive_name}")
-        run(f"sudo rm -rf /data/web_static/current")
-        run(f"sudo ln -s {remote_path} /data/web_static/current")
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}{}/".format(path, no_ext))
+        run("tar -xzf /tmp/{} -C {}{}/".format(file_n, path, no_ext))
+        run("rm /tmp/{}".format(file_n))
+        run("mv {0}{1}/web_static/* {0}{1}/".format(path, no_ext))
+        run("rm -rf {}{}/web_static".format(path, no_ext))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}{}/ /data/web_static/current".format(path, no_ext))
         return True
     except:
         return False
 
 
 def deploy():
-    """
-    creates and distributes an archive to your web servers
-    """
-    f_path = do_pack()
-    if f_path is None:
+    """creates and distributes an archive to the web servers"""
+    archive_path = do_pack()
+    if archive_path is None:
         return False
-    return do_deploy(f_path)
+    return do_deploy(archive_path)
