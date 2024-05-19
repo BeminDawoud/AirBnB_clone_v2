c86c04c9a0e3b3548937fc7323d7bfb7238acdce
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-05-19 06:20:58 +0300

    Create 3-python is cool

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Flask is awesome
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    '''flask app'''
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''Second route'''
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_cool(text):
+    '''Second route'''
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text='is cool'):
+    '''Second route'''
+    return f"Python {text.replace('_', ' ')}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
