2acfc7ca2a36e671488f635ef2271d49f40a3528
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-05-19 06:03:34 +0300

    Create 1-hbnb_route

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This is to run a minimal app using flask
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    '''flask app'''
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello():
+    '''flask app'''
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
