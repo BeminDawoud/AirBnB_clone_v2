b758c19681a8da593436e605c1913846fdf226ea
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-05-19 07:01:35 +0300

    UPdate templates

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -44,7 +44,7 @@ def number_template(n):
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_even(n):
-    if n / 2 == 0:
+    if n % 2 == 0:
         odds = "odd"
     else:
         odds = "even"
